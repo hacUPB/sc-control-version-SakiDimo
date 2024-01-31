e89822c56dab02d1a5f37e696ae2ea1bee0ae74d
Saki <143814407+SakiDimo@users.noreply.github.com>
2024-01-31 15:40:09 +0000

    Correccion de error

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -8,8 +8,9 @@ int main(int argc, char *argv[]) {
     }
     
     int number = atoi(argv[1]); //Convert the console input argument to an integer
-    
+
     /*It must be verified that the entered number is greater than or equal to zero*/
+    
     if(number < 0)
     {
         perror("The number must be greater than or equal to zero\n");
